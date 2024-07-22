8285442e744ccd0241591d51a1b4edb51d8de0be
akist529 <akist0707@gmail.com>
2024-07-22 14:51:48 -0500

    Restructure navbar component to use syntactically valid HTML structure

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -5,25 +5,27 @@ export default function Navbar(props: any) {
     const { setDisplayDrawer } = props;
 
     return (
-        <nav className="flex justify-between items-center px-5 py-5 sm:px-10 text-2xl">
+        <header className="flex justify-between items-center px-5 py-5 sm:px-10 text-2xl">
             <a href="/">
                 <AiFillBank className="flex-initial size-12 lg:size-16" />
             </a>
-            <ul className="flex-auto justify-center items-center hidden lg:flex">
-                <li className="flex flex-initial justify-center px-5">
-                    <a href="/">Home</a>
-                </li>
-                <li className="flex flex-initial justify-center px-5">
-                    <a href="/budgets">Budgets</a>
-                </li>
-                <li className="flex flex-initial justify-center px-5">
-                    <a href="/expenses">Expenses</a>
-                </li>
-            </ul>
-            <a className="flex-initial hidden lg:block" href="/login">Log In</a>
+            <nav className="flex-auto hidden lg:flex justify-between items-center">
+                <ul className="flex-1 justify-center items-center hidden lg:flex">
+                    <li className="flex flex-initial justify-center px-5">
+                        <a href="/">Home</a>
+                    </li>
+                    <li className="flex flex-initial justify-center px-5">
+                        <a href="/budgets">Budgets</a>
+                    </li>
+                    <li className="flex flex-initial justify-center px-5">
+                        <a href="/expenses">Expenses</a>
+                    </li>
+                </ul>
+                <a className="flex-initial hidden lg:block" href="/login">Log In</a>
+            </nav>
             <button onClick={() => setDisplayDrawer(true)}>
                 <SlMenu className="flex-initial lg:hidden size-8 lg:size-32" />
             </button>
-        </nav>
+        </header>
     );
 }
