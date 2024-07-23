727e0ac7c3fd8204ca72fcbca7ef652e6e3d82fb
akist529 <akist0707@gmail.com>
2024-07-22 19:27:59 -0500

    Replace links with NavLink components and alter various styling for navbar and drawer components

--- app/components/drawer.tsx
+++ app/components/drawer.tsx
@@ -1,34 +1,80 @@
+import { NavLink } from "@remix-run/react";
 import { AiFillBank } from "@icons/ai";
 import { CgClose } from "@icons/cg";
+import { MdDarkMode, MdOutlineDarkMode } from "@icons/md";
 
 export default function Drawer(props: any) {
     const { setDisplayDrawer } = props;
 
     return (
-        <div id="drawer" className="absolute grid grid-rows-2 grid-cols-1 justify-items-center bg-white w-full p-5">
-            <button className="absolute right-2 top-2" onClick={() => setDisplayDrawer(false)}>
-                <CgClose className="size-8 icon-violet-500" />
-            </button>
-            <a className="flex items-center gap-1 text-violet-500 text-4xl font-bold" href="/">
-                <AiFillBank className="size-12 icon-violet-500" />
-                BUDGET
-            </a>
-            <nav className="w-full">
-                <ul className="flex flex-col gap-5">
-                    <li>
-                        <a href="/login" onClick={() => setDisplayDrawer(false)}>Login</a>
-                    </li>
-                    <li>
-                        <a href="/" onClick={() => setDisplayDrawer(false)}>Home</a>
-                    </li>
-                    <li>
-                        <a href="/budgets" onClick={() => setDisplayDrawer(false)}>Budgets</a>
-                    </li>
-                    <li>
-                        <a href="/expenses" onClick={() => setDisplayDrawer(false)}>Expenses</a>
-                    </li>
-                </ul>
-            </nav>
+        <div id="drawer" className="absolute grid grid-rows-{auto_1fr} gap-5 bg-white w-full p-5">
+            <div id="drawer_header">
+                <button className="absolute right-2 top-2" onClick={() => setDisplayDrawer(false)}>
+                    <CgClose className="size-8 icon-violet-500" />
+                </button>
+                <NavLink
+                    to="/"
+                    className="flex justify-center items-center gap-1 text-violet-500 text-4xl font-bold"
+                >
+                    <AiFillBank className="size-12 icon-violet-500" />
+                    BUDGET
+                </NavLink>
+            </div>
+            <div id="drawer_content" className="flex flex-col gap-1">
+                <nav>
+                    <ul className="flex flex-col gap-1">
+                        <li className="flex justify-start align-center">
+                            <NavLink
+                                to="/login"
+                                onClick={() => setDisplayDrawer(false)}
+                                className={({ isActive, isPending }) => isActive ? 
+                                    "w-full p-4 bg-slate-200 text-violet-500 font-bold rounded-md" : 
+                                    "w-full p-4 active:bg-slate-200 hover:bg-slate-100 rounded-md"}
+                            >Log in</NavLink>
+                        </li>
+                        <li className="flex justify-start align-center">
+                            <NavLink
+                                to="/signup"
+                                onClick={() => setDisplayDrawer(false)}
+                                className={({ isActive, isPending }) => isActive ? 
+                                    "w-full p-4 bg-slate-200 text-violet-500 font-bold rounded-md" : 
+                                    "w-full p-4 active:bg-slate-200 hover:bg-slate-100 rounded-md"}
+                            >Sign up</NavLink>
+                        </li>
+                        <li className="flex justify-start align-center">
+                            <NavLink
+                                to="/"
+                                onClick={() => setDisplayDrawer(false)}
+                                className={({ isActive, isPending }) => isActive ? 
+                                    "w-full p-4 bg-slate-200 text-violet-500 font-bold rounded-md" : 
+                                    "w-full p-4 active:bg-slate-200 hover:bg-slate-100 rounded-md"}
+                            >Home</NavLink>
+                        </li>
+                        <li className="flex justify-start align-center">
+                            <NavLink
+                                to="/budgets"
+                                onClick={() => setDisplayDrawer(false)}
+                                className={({ isActive, isPending }) => isActive ? 
+                                    "w-full p-4 bg-slate-200 text-violet-500 font-bold rounded-md" : 
+                                    "w-full p-4 active:bg-slate-200 hover:bg-slate-100 rounded-md"}
+                            >Budgets</NavLink>
+                        </li>
+                        <li className="flex justify-start align-center">
+                            <NavLink
+                                to="/expenses"
+                                onClick={() => setDisplayDrawer(false)}
+                                className={({ isActive, isPending }) => isActive ? 
+                                    "w-full p-4 bg-slate-200 text-violet-500 font-bold rounded-md" : 
+                                    "w-full p-4 active:bg-slate-200 hover:bg-slate-100 rounded-md"}
+                            >Expenses</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <button className="text-white bg-violet-500 p-4 rounded-md flex items-center gap-1">
+                    Dark Mode
+                    <MdOutlineDarkMode className="size-6 icon-white" />
+                </button>
+            </div>
         </div>
     );
 }
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,40 +1,65 @@
+import { NavLink } from "@remix-run/react";
 import { AiFillBank } from "@icons/ai";
 import { SlMenu } from "@icons/sl";
 import { BiLogIn } from "@icons/bi";
-import { MdDarkMode, MdOutlineDarkMode } from "@icons/md";
+import { MdDarkMode, MdOutlineDarkMode, MdAccountBox } from "@icons/md";
 
 export default function Navbar(props: any) {
     const { setDisplayDrawer } = props;
 
     return (
-        <header className="flex justify-evenly items-center gap-5 px-5 py-5 sm:px-10">
+        <header className="grid grid-cols-2 lg:grid-cols-3 px-5 py-5 sm:px-10">
             <div className="flex-1 flex justify-start items-center gap-5">
-                <a className="flex items-center gap-1 text-violet-500 text-4xl font-bold" href="/">
+                <NavLink
+                    to="/"
+                    className="flex items-center gap-1 text-violet-500 text-4xl font-bold"
+                >
                     <AiFillBank className="flex-initial size-12 icon-violet-500" />
                     BUDGET
-                </a>
+                </NavLink>
                 <button className="hidden lg:block">
                     <MdOutlineDarkMode className="size-12 icon-violet-500" />
                 </button>
             </div>
             <nav className="flex-1 hidden lg:flex justify-between items-center text-xl">
                 <ul className="flex-1 justify-center items-center hidden lg:flex">
-                    <li className="flex flex-initial justify-center px-5">
-                        <a href="/">Home</a>
+                    <li className="flex flex-initial justify-center hover:bg-slate-100 active:bg-slate-200 rounded-3xl">
+                        <NavLink
+                            to="/"
+                            className="px-5 py-2"
+                        >Home</NavLink>
                     </li>
-                    <li className="flex flex-initial justify-center px-5">
-                        <a href="/budgets">Budgets</a>
+                    <li className="flex flex-initial justify-center hover:bg-slate-100 active:bg-slate-200 rounded-3xl">
+                        <NavLink
+                            to="/budgets"
+                            className="px-5 py-2"
+                        >Budgets</NavLink>
                     </li>
-                    <li className="flex flex-initial justify-center px-5">
-                        <a href="/expenses">Expenses</a>
+                    <li className="flex flex-initial justify-center hover:bg-slate-100 active:bg-slate-200 rounded-3xl">
+                        <NavLink
+                            to="/expenses"
+                            className="px-5 py-2"
+                        >Expenses</NavLink>
                     </li>
                 </ul>
             </nav>
-            <a className="flex-1 lg:flex justify-end items-center hidden" href="/login">
-                Log In
-                <BiLogIn className="size-6 icon-black" />
-            </a>
-            <button onClick={() => setDisplayDrawer(true)}>
+            <div className="flex-1 lg:flex justify-end items-center gap-5 hidden">
+                <NavLink
+                    to="/login"
+                    className="flex justify-end items-center hover:bg-slate-100 active:bg-slate-200 rounded-3xl px-5 py-2"
+                >
+                    Log in
+                    <BiLogIn className="size-6 icon-black" />
+                </NavLink>
+                <NavLink
+                    to="/signup"
+                    className="bg-violet-500 text-white rounded-3xl flex items-center gap-1 hover:bg-violet-400 px-5 py-2"
+                >
+                    Sign up
+                    <MdAccountBox className="size-6 icon-white" />
+                </NavLink>
+            </div>
+            <button className="flex justify-end items-center" onClick={() => setDisplayDrawer(true)}>
                 <SlMenu className="flex-initial lg:hidden size-8 lg:size-32 icon-violet-500" />
             </button>
         </header>
